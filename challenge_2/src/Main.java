e663cb0a50c3b59921166c990afd4bbdc38506e4
haiemdavang <lengochai7304@gmail.com>
2025-09-20 18:15:22 +0700

    challenge_2

--- challenge_2/src/Main.java
+++ challenge_2/src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static int factorial(int n) {
+        if (n <= 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static int countFrequent(int count) {
+        return factorial(count)/(2*factorial(count-2));
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        String s = input.next();
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (n-- <= 0)
+                break;
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
+
+        int result = 0;
+        for (int count : map.values())
+            result += countFrequent(count);
+
+        System.out.println(result);
+
     }
 }
